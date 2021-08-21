e7503f1759c85f293f2cac3ed80e12a95a3846cc
ray_arwindra <wan.muhammad81@ui.ac.id>
2021-08-21 18:14:01 +0700

    Added alt to image

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,67 +1,70 @@
 import {
-    AppBar, 
-    Toolbar, 
-    IconButton,
-    BottomNavigation,
-    BottomNavigationAction
-} from "@material-ui/core"
+  AppBar,
+  Toolbar,
+  IconButton,
+  BottomNavigation,
+  BottomNavigationAction,
+} from "@material-ui/core";
 
 import {
-    FavoriteBorder, 
-    ShoppingCartOutlined, 
-    AccountCircleOutlined,
-    HomeOutlined,
-    HistoryOutlined
-} from "@material-ui/icons"
+  FavoriteBorder,
+  ShoppingCartOutlined,
+  AccountCircleOutlined,
+  HomeOutlined,
+  HistoryOutlined,
+} from "@material-ui/icons";
 
 import Link from "next/link";
-import Image from 'next/image'
-import logo from '../public/logo.png'
-import styles from "../styles/components/Navbar.module.scss"
+import Image from "next/image";
+import logo from "../public/logo.png";
+import styles from "../styles/components/Navbar.module.scss";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <AppBar className={styles.navbar} color="transparent" position="static">
-                <Toolbar>
-                    <span className={styles.navbar_logo}>
-                        <Image src={logo}/>
-                    </span>
-                    <div>
-                        <span className={styles.navbar_searchbar_icon}/>
-                        <input className={styles.navbar_searchbar} placeholder="Search your favourite vegies here..."/>
-                    </div>
-                    <div className={styles.navbar_items}>
-                        <Link href='/'>
-                            <a> Home </a>
-                        </Link>
-                        <Link href='/history'>
-                            <a> History </a>
-                        </Link>
-                        <IconButton>
-                            <FavoriteBorder />
-                        </IconButton>
-                        <IconButton>
-                            <ShoppingCartOutlined />
-                        </IconButton>
-                        <IconButton>
-                            <AccountCircleOutlined />
-                        </IconButton>
-                    </div>
-                    <div className={styles.navbar_shoppingCart}>
-                        <IconButton>
-                                <ShoppingCartOutlined />
-                        </IconButton>
-                    </div>
-                </Toolbar>
-            </AppBar>
+  return (
+    <nav>
+      <AppBar className={styles.navbar} color="transparent" position="static">
+        <Toolbar>
+          <span className={styles.navbar_logo}>
+            <Image src={logo} alt="logo" />
+          </span>
+          <div>
+            <span className={styles.navbar_searchbar_icon} />
+            <input
+              className={styles.navbar_searchbar}
+              placeholder="Search your favourite vegies here..."
+            />
+          </div>
+          <div className={styles.navbar_items}>
+            <Link href="/">
+              <a> Home </a>
+            </Link>
+            <Link href="/history">
+              <a> History </a>
+            </Link>
+            <IconButton>
+              <FavoriteBorder />
+            </IconButton>
+            <IconButton>
+              <ShoppingCartOutlined />
+            </IconButton>
+            <IconButton>
+              <AccountCircleOutlined />
+            </IconButton>
+          </div>
+          <div className={styles.navbar_shoppingCart}>
+            <IconButton>
+              <ShoppingCartOutlined />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
 
-            <BottomNavigation className={styles.navbar_mobile}>
-                <BottomNavigationAction icon={<HomeOutlined />} />
-                <BottomNavigationAction icon={<FavoriteBorder />} />
-                <BottomNavigationAction icon={<HistoryOutlined />} />
-                <BottomNavigationAction icon={<AccountCircleOutlined />} />
-            </BottomNavigation>
-        </nav>
-    )
+      <BottomNavigation className={styles.navbar_mobile}>
+        <BottomNavigationAction icon={<HomeOutlined />} />
+        <BottomNavigationAction icon={<FavoriteBorder />} />
+        <BottomNavigationAction icon={<HistoryOutlined />} />
+        <BottomNavigationAction icon={<AccountCircleOutlined />} />
+      </BottomNavigation>
+    </nav>
+  );
 }

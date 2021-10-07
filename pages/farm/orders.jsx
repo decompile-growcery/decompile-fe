6ce9b5274bc281012a11fb58f0c7327f2645dc71
Tsamara Esperanti Erwin <sarah.esperanti@gmail.com>
2021-10-07 19:33:02 +0700

    console log on order error

--- pages/farm/orders.jsx
+++ pages/farm/orders.jsx
@@ -2,18 +2,19 @@ import { useEffect, useState } from "react";
 import Head from "next/head";
 import OrderItem from "../../components/orderItem";
 import useUser from "../../lib/hooks/useUser";
-import styles from "../../styles/pages/Cart.module.scss";
+import styles from "../../styles/pages/Order.module.scss";
 import FarmerNavbar from "../../components/farmNavbar";
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 export default function Cart() {
   const user = useUser();
   const [orderList, setOrders] = useState([]);
-  const [checkedAll, setCheckedAll] = useState(false);
-
+  
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_LINK}farm-orders`, {
       headers: {
-        // Authorization: `Bearer ${user}`,
         Authorization: `Bearer ${user}`,
       },
     })
@@ -23,18 +24,41 @@ export default function Cart() {
       );
   }, []);
 
+  const filterOrder = (id) => {
+    const newOrders = orderList.filter((d) => d.status_id == id);
+    setOrders(newOrders);
+  }
+  
   return (
     <div>
       <Head>
         <title>Farm Orders | Growcery</title>
       </Head>
       <FarmerNavbar />
-      <main className={styles.cart}>
+      <main className={styles.orders}>
         <div>
           <h1>  My Orders  </h1>
         </div>
 
-        <div className={styles.cart_container}>
+        <div className={styles.orders_container}>
+          
+            <Grid container>
+              <Grid item xs={3}> 
+              </Grid>
+              <Grid item xs={3}> 
+              </Grid>
+              <Grid item xs={6}>
+                <div className={styles.orders_statusButtons}>
+                  <ButtonGroup variant="text" aria-label="text primary button group">
+                    <Button onClick={() => filterOrder(1)}>Waiting For Payment</Button>
+                    <Button onClick={() => filterOrder(2)}>Payment Confirmed</Button>
+                    <Button onClick={() => filterOrder(3)}>Shipped</Button>
+                    <Button onClick={() => filterOrder(4)}>Completed</Button>
+                  </ButtonGroup>
+                </div>
+              </Grid>
+            </Grid>
+          
             {orderList.map((c) => (
               <OrderItem
                 key = {c.order_id}

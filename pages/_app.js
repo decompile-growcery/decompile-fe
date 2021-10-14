801cc3a32a5563463fe24ba1d04342b77a0744c7
fbunaren <fransiscusbunaren@gmail.com>
2021-10-14 22:19:29 +0700

    Implement serviceWorker, Manifest.json and adding PNGs

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
-        />
+        />		
       </Head>
         <Component {...pageProps} />
     </>

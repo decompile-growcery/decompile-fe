42ed6e0041973fb73e9f7c5b8114f9ccf8a6989d
fbunaren <fransiscusbunaren@gmail.com>
2021-10-21 05:53:02 +0700

    Load Manifest.json for PWA

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+		<link rel="manifest" href="manifest.json"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
